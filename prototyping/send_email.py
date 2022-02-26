ef17a50f6cadf440d714f16f25252d95f20bf12c
Your Name <you@example.com>
2022-02-26 23:59:53 +0100

    mail test

--- prototyping/send_email.py
+++ prototyping/send_email.py
@@ -4,35 +4,35 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pathlib import Path
-
-app_generated_password='saxxdxfygdcxhzaq'
-my_address = "mats.bohlinsson@gmail.com"  # sender address
-to_address= my_address
-
 import smtplib
-from email.message import EmailMessage
-
-msg = MIMEMultipart()
 
 
 
-msg["Subject"] = "The Email Subject"  # email subject
-msg["From"] = my_address  # sender address
-msg["To"] = to_address  # reciver address
-msg.attach(MIMEText("Hello", 'plain'))
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-    smtp.login(my_address, app_generated_password)  # login gmail account
-    print("sending mail")
+def send_attachment(subject: str, message:str, files:[Path], app_generated_password='saxxdxfygdcxhzaq', from_address ="mats.bohlinsson@gmail.com", to_address= None):
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    msg["From"] = from_address  # sender address
+    msg["To"] = to_address  # reciver address
+    msg.attach(MIMEText("Hello", message))
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(from_address, app_generated_password)  # login gmail account
 
-    file_path = "send_email.py"
-    mimeBase = MIMEBase("application", "octet-stream")
-    with open(file_path, "rb") as file:
-        mimeBase.set_payload(file.read())
-    encoders.encode_base64(mimeBase)
-    mimeBase.add_header("Content-Disposition", f"attachment; filename={Path(file_path).name}")
-    msg.attach(mimeBase)
+        for file in files:
+            file_path = file.name
+            mimeBase = MIMEBase("application", "octet-stream")
+            with open(file_path, "rb") as fileread:
+                mimeBase.set_payload(fileread.read())
+            encoders.encode_base64(mimeBase)
+            mimeBase.add_header("Content-Disposition", f"attachment; filename={Path(file_path).name}")
+            msg.attach(mimeBase)
 
-    smtp.send_message(msg)  # send message
-    print("mail has sent")
+        smtp.send_message(msg)  # send message
+        print("mail has sent")
 
-
+if __name__ == "__main__":
+    send_attachment(subject="subject",
+                    message='Läget?',
+                    files=[Path('send_email.py'), Path('openapi.json')],
+                    app_generated_password = 'saxxdxfygdcxhzaq',
+                    from_address="mats.bohlinsson@gmail.com",
+                    to_address = "mats.bohlinsson@gmail.com")
