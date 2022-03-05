db61f2c6d11c7e80dc212d5cb4a9e6cbb8e667c6
Your Name <you@example.com>
2022-03-05 20:42:38 +0100

    fastapi test

--- app/startup/main.py
+++ app/startup/main.py
@@ -57,7 +57,8 @@ def startup(android_activity):
         _thread.start_new_thread(startup_remi, (android_activity,))
         #start_autostart()
         import DroneSdk.sdk
-        uvicorn.run("DroneSdk.sdk:app", host='0.0.0.0', port=4557, reload=False, debug=False, workers=3)
+        uvicorn.run("DroneSdk.sdk:app", host='0.0.0.0', port=4557)
+        #uvicorn.run("DroneSdk.sdk:app", host='0.0.0.0', port=4557, reload=False, debug=False, workers=3)
 
     except:
         print(traceback.format_exc())
