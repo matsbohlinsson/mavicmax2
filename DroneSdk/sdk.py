b96ce55f23670e28e0776e19d2bb5182767272bb
Your Name <you@example.com>
2022-03-05 21:03:42 +0100

    fastapi test

--- DroneSdk/sdk.py
+++ DroneSdk/sdk.py
@@ -16,29 +16,29 @@ else:
 app_fastapi = FastAPI(title='MavicMax', version='1.0')
 
 
-@app_fastapi.post("get_drone_telemetry", response_model=models.Telemetry)
+@app_fastapi.post("/get_drone_telemetry", response_model=models.Telemetry)
 def get_drone_telemetry() -> models.Telemetry:
     return current_sdk.get_telemetry()
 
-@app_fastapi.post("get_rc_telemetry", response_model=models.Rc)
+@app_fastapi.post("/get_rc_telemetry", response_model=models.Rc)
 def get_rc_telemetry() -> models.Rc:
     return current_sdk.get_rc_inputs()
 
 
 #@app.post("start_simulator/{lat}/{lon}")
-@app_fastapi.post("start_simulator")
+@app_fastapi.post("/start_simulator")
 def start_simulator(self, lat:float = 58.1111, lon: float=11.010203):
     current_sdk.start_simulator(lat, lon)
 
-@app_fastapi.post("update_url_touch")
+@app_fastapi.post("/update_url_touch")
 def update_url_touch(url:str):
     current_sdk.update_url_touch(url)
 
-@app_fastapi.post("get_log_dir")
+@app_fastapi.post("/get_log_dir")
 def get_log_dir():
     return current_sdk.get_log_dir()
 
-@app_fastapi.post("test")
+@app_fastapi.post("/test")
 def fastapi_test():
     return "test_ok"
 
